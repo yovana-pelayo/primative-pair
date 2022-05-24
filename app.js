0fdc33e988cf65bbaf835b2bb0cc0dca42d0ed28
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-05-24 15:23:58 -0700

    spotlight

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-// import functions and grab DOM elements
+/* eslint-disable */
 
-// let state
+// Challenge1:
+function reverseWords(sentence) {
+    return sentence
+        .split(' ')
+        .map((word) => word.split('').reverse().join(''))
+        .join('');
+}
+reverseWords('alchemy rocks gold');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+//Challenge2:
+function titleCase(sentence) {
+    return sentence
+        .split('')
+        .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+        .join('');
+}
+titleCase('alchemy ROCKS goLD');
+
+//Challenge3:
+function oddishOrEvenish(number) {
+    // const nums= number.split('');
+    // const initial= 0;
+    // const sum = nums.reduce(previous, current) => previous + current, initial );
+    const sum = number
+        .toString()
+        .split('')
+        .reduce((all, n) => {
+            all += Number(n);
+            return all;
+        }, 0);
+    return sum % 2 === 0 ? 'Evenish' : 'Oddish';
+}
+oddishOrEvenish(42);
+oddishOrEvenish(121);
+
+function anagrams(wordOne, wordTwo) {
+    let word1 = wordOne.split('').sort().join();
+    let word2 = wordTwo.split('').sort().join();
+    const results = word1 === word2 ? 'true' : 'false';
+    // console.log('results', results);
+    return results;
+}
+anagrams('superintended', 'unpredestined');
+anagrams('pictorialness', 'documentarily');
+anagrams('Aidin', 'dinAi');
+anagrams('qoewhrwo', 'sdfjghrwoghfqo');
